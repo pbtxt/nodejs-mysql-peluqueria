e287575c7fcecc3291b38cb0fa8b6db9f638dee1
Haner Avella <haneravella@Haners-MacBook-Pro.local>
2020-01-14 19:20:03 -0500

    Archivo donde se anade scrpit con tratamientos

---
+++ database/scripts.sql
@@ -0,0 +1,8 @@
+INSERT INTO tratamientos
+    (nombre_tratamiento, duracion_tratamiento, costo_tratamiento)
+VALUES
+    ("Corte-Mujer", 30, 35),
+    ("Corte-Hombre", 30, 20),
+    ("Barba", 30, 20),
+    ("Cejas", 30, 15),
+    ("Tinte", 60, 50)
