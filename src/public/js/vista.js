fdb5e27be0acdfe352ccfdaba70209fad521b615
Haner Avella <haneravella@Haners-MacBook-Pro.local>
2020-02-25 00:52:10 -0500

    Agrega comentario para realizar cmabio de una de las imagenes del carousel

--- src/public/js/vista.js
+++ src/public/js/vista.js
@@ -1,13 +1,1 @@
-/* 
-        $("button").click(function () {
-            console.log('print')
-            //{{!-- $("p").removeClass("intro"); --}}
-        });
-        */
-// $(document).ready(function() {
-$('button').click(function() {
-	console.log('print');
-	$('#navbarSupportedContent ul').toggleClass('offset-md-7');
-	//$("p:last").removeClass("intro").addClass("main");
-	// });
-});
+
